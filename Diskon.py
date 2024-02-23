e434efa29fc870f565738c198052d3d5994a1376
RendiRuswandi <151483636+RendiRuswandi@users.noreply.github.com>
2024-02-23 11:16:42 +0700

    Add files via upload

---
+++ Diskon.py
@@ -0,0 +1,6 @@
+TotalBelanja = int (input("Berapa Total Blenja : "))
+
+if (TotalBelanja >= 60000):
+    hargaAkhir = TotalBelanja - 10000
+    print("Selamat anda mendapatkan diskon 10000")
+    print("totalBayar = RP. ",round(hargaAkhir))
---
+++ Menghitung Uang Kembalian.py
@@ -0,0 +1,5 @@
+uang = int(input("Berapa uang saya: "))
+harga = 50000
+kembalian = uang - harga
+
+print("kembalian %i, dari uang %i"% (kembalian, uang))
