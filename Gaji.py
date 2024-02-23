6e2f10beff24258e7b80021ec4eb6f2a536dc144
RendiRuswandi <151483636+RendiRuswandi@users.noreply.github.com>
2024-02-22 22:47:10 -0800

    Gaji.py

--- Gaji.py
+++ Gaji.py
@@ -2,12 +2,12 @@ gajiBulanan = int(input("Gaji bulanan : "))
 masukKerja = int(input("Berapa hari anda masuk kerja : "))
 uangTransport = 100000 * masukKerja
 uangMakan = 50000 * masukKerja
-hariLembur = int(input("Berapa jam kerja lembur anda : "))
+jamLembur = int(input("Berapa jam kerja lembur anda : "))
 
-if hariLembur <= 2:
-    totalLembur = hariLembur * 100000
+if jamLembur <= 2:
+    totalLembur = jamLembur * 100000
 else : 
-    totalLembur = 2 * 100000 + (hariLembur - 2) * 150000
+    totalLembur = 2 * 100000 + (jamLembur - 2) * 150000
 honor = gajiBulanan+uangTransport+uangMakan+totalLembur
 
 print("Uang honor yang didapat Rp.%i"%honor)
